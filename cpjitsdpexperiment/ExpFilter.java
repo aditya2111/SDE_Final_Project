f8179eeb7bb827230ee017dccd7d93f136af6899
aditya2111 <56698471+aditya2111@users.noreply.github.com>
2022-04-22 03:27:27 +0530

    Add files via upload

---
+++ cpjitsdpexperiment/ExpFilter.java
@@ -0,0 +1,77 @@
+
+package cpjitsdpexperiment;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.concurrent.TimeUnit;
+
+import moa.options.ClassOption;
+import moa.tasks.EvaluatePrequential;
+import moa.tasks.MainTask;
+import moa.tasks.TaskThread;
+
+public class ExpFilter {
+
+	static MainTask currentTask = new EvaluatePrequential();
+	static Writer writer;
+
+	public ExpFilter() {
+
+	}
+
+	private static String[] savedArgs;
+
+	public static String[] getArgs() {
+		return savedArgs;
+	}
+	public static void main(String[] args) throws IOException, InterruptedException {
+
+		savedArgs = args;
+
+		int dsIdx = new Integer(args[0]);
+		int arrId = new Integer(args[1]);
+		String ens =  args[2];
+		String theta = args[3];
+		String waitingTime= args[4];
+		String filterParams = args[5];
+		/*** Use only for ORB ***/
+		String paramsORB=args[6];
+
+
+		//		0
+		//		0
+		//		20
+		//		0.99
+		//		90
+		//		500;50;0.7;500
+		//		100;0.4;10;12;1.5;3
+
+		String[] datasetsArray = {"tomcat","JGroups","spring-integration",
+				"camel","brackets","nova","fabric8",
+				"neutron","npm","BroadleafCommerce"
+		};
+
+
+		/*** OOB ***/
+		//		 String task = "CpjitsdpFiltering  -l (meta.ggc2.meta.WaitForLabelsOOB -i 15 -s "+ens+" -t "+theta+" -w "+waitingTime+")  -s  (ArffFileStream -f (datasets/"+datasetsArray[dsIdx]+".arff) -c 15) -e (FadingFactorEachClassPerformanceEvaluator -a 0.99) -f 1 -d results/"+datasetsArray[dsIdx]+"-Filter-OOB-"+filterParams.replaceAll(";", "-")+"-"+arrId+".csv";
+
+		/*** ORB ***/
+		String task = "CpjitsdpFiltering -l (spdisc.meta.WFL_OO_ORB_Oza -i 15 -s "+ens+" -t "+theta+" -w "+waitingTime+" -p "+paramsORB+")  -s  (ArffFileStream -f (datasets/"+datasetsArray[dsIdx]+".arff) -c 15) -e (FadingFactorEachClassPerformanceEvaluator -a 0.99) -f 1 -d results/"+datasetsArray[dsIdx]+"-Filter-ORB-("+paramsORB.replaceAll(";", "-")+")-"+filterParams.replaceAll(";", "-")+"-"+arrId+".csv";
+
+		try {
+
+			System.out.println(task);
+			currentTask = (MainTask) ClassOption.cliStringToObject(task, MainTask.class, null);
+
+			TaskThread thread = new TaskThread((moa.tasks.Task) currentTask);
+
+			thread.start();
+
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+	}
+
+}
